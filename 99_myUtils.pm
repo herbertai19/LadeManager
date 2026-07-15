f02f308c4f97048885dbed7101fd15d9184d25df
herbertai19 <herbert@aeckerle.com>
2026-07-15 19:57:46 +0200

    Update 99_myUtils.pm

--- 99_myUtils.pm
+++ 99_myUtils.pm
@@ -17,8 +17,8 @@ sub LMLog
     my ($text) = @_;
 
     my $ts = TimeNow();
-
-    Log 1,"[LM] $text";
+        LMLog("LadeManager: $car bereits auf Ziel.");
+    LMLog("[LM] $text");
 
     if (open(my $fh, ">>", "/opt/fhem/log/LadeManager.log"))
     {
